ffeffc42684916fb859207da9b7ce8e2fb4e1fab
Daniel Iturralde Benito <tsunami56@MBP-de-Simon.home>
2024-03-11 18:43:45 +0100

    primer ejercicio

---
+++ fizz_buzz.py/fizzbuzz.py
@@ -0,0 +1,17 @@
+# Ejercicio FizzFuzz
+# Autor: Daniel Iturralde
+# Fecha: 2024/03/11
+
+#Variable
+n = 0
+
+# Se define la función fizzfuzz
+def fizzbuzz(n):
+    while n <= 100:
+        n += 1
+        if n % 3 == 0 and n % 5 == 0:
+            print('FizzBuzz')
+        elif n  % 3 == 0:
+            print ('Fizz')
+        elif n % 5 == 0:
+            print ('Buzz')
